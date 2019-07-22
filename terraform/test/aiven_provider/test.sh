1fb10cecc20f6039fbbe31cb3cf87d8f9bba0805
Daniel Flook <daniel.flook@ovoenergy.com>
2019-07-22 15:21:48 +0100

    Add AIVEN_PROVIDER tests

---
+++ terraform/test/aiven_provider/test.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir -p .circleci
+
+sed -e "s|ovotech/terraform@1|ovotech/terraform@dev:${USER}_test|" config.yml | circleci config process - >.circleci/config.yml
+
+function test_case() {
+    local JOB=$1
+    local VERSION=$2
+
+    circleci local execute --job $JOB | tee /tmp/output
+
+    if [[ "$(tail -n 1 /tmp/output)" == *"Success"* ]]; then
+        echo "Job $JOB Success"
+    else
+        echo "Job $JOB Fail"
+        exit 1
+    fi
+
+    if ! grep "$VERSION" /tmp/output > /dev/null; then
+        echo "Failed test $JOB"
+        exit 2
+    fi
+}
+
+test_case terraform_11_aiven "provider.aiven v1.0.10"
+test_case terraform_11 "provider.aiven v0.0.1"
+test_case terraform_12 "provider.aiven v1.0.10"
+
+echo "All tests passed"
