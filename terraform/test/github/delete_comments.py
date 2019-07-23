38d7565cd8eb0d63f02cdad7cbe538662a48c9da
Daniel Flook <daniel.flook@ovoenergy.com>
2019-07-23 12:10:56 +0100

    Fail test if CIRCLE_PR_NUMBER not set

--- terraform/test/github/delete_comments.py
+++ terraform/test/github/delete_comments.py
@@ -7,15 +7,17 @@ github_username = os.environ['GITHUB_USERNAME']
 github_token = os.environ['GITHUB_TOKEN']
 owner = os.environ['CIRCLE_PROJECT_USERNAME']
 repo = os.environ['CIRCLE_PROJECT_REPONAME']
-pr_number = os.environ.get('CIRCLE_PR_NUMBER')
+pr_number = os.environ['CIRCLE_PR_NUMBER']
 
 github = requests.Session()
 github.auth = (github_username, github_token)
 
 response = github.get(f'https://api.github.com/repos/{owner}/{repo}/pulls/{pr_number}')
 response.raise_for_status()
+
 response = github.get(response.json()['url'])
 response.raise_for_status()
+
 issue_url = response.json()['_links']['issue']['href'] + '/comments'
 response = github.get(issue_url)
 response.raise_for_status()
