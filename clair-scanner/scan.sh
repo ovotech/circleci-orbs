aae77a28a3b852c8f814ff92246b1418cabac7d8
eversC <chris.every@ovoenergy.com>
2019-09-18 11:16:51 +0100

    Fixes for shellcheck

--- clair-scanner/scan.sh
+++ clair-scanner/scan.sh
@@ -4,44 +4,45 @@ set -ex
 
 if [ -z "<< parameters.image_file >><< parameters.image >>" ]; then
     echo "Either the image_file or image parameters must be present"
-    exit -1
+    exit 255
 fi
 
 REPORT_DIR=/clair-reports
 mkdir $REPORT_DIR
 
 DB=$(docker run -p 5432:5432 -d arminc/clair-db:latest)
-CLAIR=$(docker run -p 6060:6060 --link $DB:postgres -d arminc/clair-local-scan:latest)
+CLAIR=$(docker run -p 6060:6060 --link "$DB":postgres -d arminc/clair-local-scan:latest)
 CLAIR_SCANNER=$(docker run -v /var/run/docker.sock:/var/run/docker.sock -d ovotech/clair-scanner:latest tail -f /dev/null)
 
-clair_ip=$(docker exec -it $CLAIR hostname -i | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
-scanner_ip=$(docker exec -it $CLAIR_SCANNER hostname -i | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
+clair_ip=$(docker exec -it "$CLAIR" hostname -i | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
+scanner_ip=$(docker exec -it "$CLAIR_SCANNER" hostname -i | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
 
 if [ -n "<< parameters.whitelist >>" ]; then
     cat "<< parameters.whitelist >>"
-    docker cp "<< parameters.whitelist >>" $CLAIR_SCANNER:/whitelist.yml
+    docker cp "<< parameters.whitelist >>" "$CLAIR_SCANNER":/whitelist.yml
 
     WHITELIST="-w /whitelist.yml"
 fi
 
 function scan() {
     local image=$1
-    local sanitised_image_filename="$(echo $image | sed 's/\//_/g').json"
+    munged_image=$(echo "$image" | sed 's/\//_/g')
+    sanitised_image_filename="${munged_image}.json"
     local ret=0
-    local docker_cmd="docker exec -it $CLAIR_SCANNER clair-scanner \
-        --ip ${scanner_ip} \
-        --clair=http://${clair_ip}:6060 \
-        -t \"<< parameters.severity_threshold >>\" \
-        --report \"/$sanitised_image_filename\" \
-        --log \"/log.json\" $WHITELIST
+    local docker_cmd=(docker exec -it "$CLAIR_SCANNER" clair-scanner \
+        --ip "$scanner_ip" \
+        --clair=http://"$clair_ip":6060 \
+        -t "<< parameters.severity_threshold >>" \
+        --report "/$sanitised_image_filename" \
+        --log "/log.json" "$WHITELIST"
         --reportAll=true \
         --exit-when-no-features=false \
-        $image"
+        "$image")
 
     docker pull "$image"
 
     if [ "<< parameters.disable_verbose_console_output >>" == "true" ];then
-        $docker_cmd > /dev/null 2>&1 || ret=$?
+        "${docker_cmd[@]}" > /dev/null 2>&1 || ret=$?
         if [ $ret -eq 0 ]; then
             echo "No unapproved vulnerabilities"
         elif [ $ret -eq 1 ]; then
@@ -58,7 +59,7 @@ function scan() {
             echo "Unknown clair-scanner return code $ret."
             EXIT_STATUS=1
         fi
-    elif ! $docker_cmd;then
+    elif ! "${docker_cmd[@]}";then
         EXIT_STATUS=1
     fi
 
@@ -70,7 +71,7 @@ EXIT_STATUS=0
 if [ -n "<< parameters.image_file >>" ]; then
     images=$(cat "<< parameters.image_file >>")
     for image in $images; do
-        scan $image
+        scan "$image"
     done
 else
     scan "<< parameters.image >>"
