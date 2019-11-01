7db4aa74d6e9bcf6fd9f25e66c8ded21ee50d76b
Ben Wart <benwart@users.noreply.github.com>
2019-11-01 11:18:55 -0700

    optionally retry auto-apply and destroy

--- terraform/apply.sh
+++ terraform/apply.sh
@@ -17,7 +17,28 @@ function update_status() {
     fi
 }
 
-function apply() {
+function plan() {
+    exec 3>&1
+
+    set +e
+    terraform plan -input=false -no-color -detailed-exitcode -out=plan.out $PLAN_ARGS "$module_path" \
+        | $TFMASK \
+        | tee /dev/fd/3 \
+        | sed '1,/---/d' \
+            >plan.txt
+
+    TF_EXIT=${PIPESTATUS[0]}
+    set -e
+
+    if [[ $TF_EXIT -eq 1 ]]; then
+        update_status "Error applying plan in CircleCI Job [${CIRCLE_JOB}](${CIRCLE_BUILD_URL})"
+        return 1
+    fi
+
+    return 0
+}
+
+function apply() {        
     set +e
     terraform apply -input=false -no-color -auto-approve plan.out | $TFMASK
     local TF_EXIT=${PIPESTATUS[0]}
@@ -25,10 +46,10 @@ function apply() {
 
     if [[ $TF_EXIT -eq 0 ]]; then
         update_status "Plan applied in CircleCI Job [${CIRCLE_JOB}](${CIRCLE_BUILD_URL})"
-        exit 0
+        return 0
     else
         update_status "Error applying plan in CircleCI Job [${CIRCLE_JOB}](${CIRCLE_BUILD_URL})"
-        exit 1
+        return 1
     fi
 }
 
@@ -40,35 +61,34 @@ fi
 
 update_status "Applying plan in CircleCI Job [${CIRCLE_JOB}](${CIRCLE_BUILD_URL})"
 
-exec 3>&1
-
-set +e
-terraform plan -input=false -no-color -detailed-exitcode -out=plan.out $PLAN_ARGS "$module_path" \
-    | $TFMASK \
-    | tee /dev/fd/3 \
-    | sed '1,/---/d' \
-        >plan.txt
-
-TF_EXIT=${PIPESTATUS[0]}
-set -e
-
-if [[ $TF_EXIT -eq 1 ]]; then
-    update_status "Error applying plan in CircleCI Job [${CIRCLE_JOB}](${CIRCLE_BUILD_URL})"
-    exit 1
-fi
-
 if [[ "<< parameters.auto_approve >>" == "true" || $TF_EXIT -eq 0 ]]; then
     echo "Automatically approving plan"
-    apply
+
+    RETRIES="<< parameters.retries >>"
+    RETRY_DELAY="<< parameters.retry_delay >>"
+
+    for ((i=0; i <= $RETRIES; i++)); do
+        if plan && apply; then 
+            break
+        fi
+
+        if [[ $i -nq $RETRIES ]]; then
+            sleep $RETRY_DELAY
+        fi
+    done
 
 else
+    if ! plan; then
+        exit 1
+    fi
+
     if ! python3 /tmp/github.py get >approved-plan.txt; then
         echo "Approved plan not found"
         exit 1
     fi
 
     if python3 /tmp/cmp.py plan.txt approved-plan.txt; then
-        apply
+        exit apply
     else
         update_status "Plan not applied in CircleCI Job [${CIRCLE_JOB}](${CIRCLE_BUILD_URL}) (Plan has changed)"
         exit 1
--- terraform/destroy.sh
+++ terraform/destroy.sh
@@ -1,1 +1,17 @@
-terraform destroy -input=false -no-color -auto-approve $PLAN_ARGS "$module_path"
+
+RETRIES="<< parameters.retries >>"
+RETRY_DELAY="<< parameters.retry_delay >>"
+
+for ((i=0; i <= $RETRIES)); do
+  TF_EXIT=terraform destroy -input=false -no-color -auto-approve $PLAN_ARGS "$module_path"
+
+  if [[ $TF_EXIT -eq 0 ]]; then
+    break
+  fi
+
+  if [[ $i -nq $RETRIES ]]; then
+    sleep $RETRY_DELAY
+  fi
+done
+
+exit $TF_EXIT
