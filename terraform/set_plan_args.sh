e1ef99c0a2d0d068cd6374c092ff1987471addd9
Daniel Flook <daniel.flook@ovoenergy.com>
2020-03-17 10:49:42 +0000

    [terraform] Support the tfvars auto loading feature
    This terraform feature doesn't really work as expected when you specify the config path in the terraform command.
    This implements the expected behaviour.
    
    Terraform will still auto load any files in the working directory before we "auto load" files in the config path.

--- terraform/set_plan_args.sh
+++ terraform/set_plan_args.sh
@@ -2,6 +2,23 @@ if [[ "<< parameters.parallelism >>" -ne 0 ]]; then
     PLAN_ARGS="$PLAN_ARGS -parallelism=<< parameters.parallelism >>"
 fi
 
+# Implement the subtly broken auto loading behaviour
+if [[ -f "$module_path/terraform.tfvars" ]]; then
+    PLAN_ARGS="$PLAN_ARGS -var-file $module_path/terraform.tfvars"
+fi
+
+if [[ -f "$module_path/terraform.tfvars.json" ]]; then
+    PLAN_ARGS="$PLAN_ARGS -var-file $module_path/terraform.tfvars.json"
+fi
+
+for file in "$module_path"/*.auto.tfvars; do
+    PLAN_ARGS="$PLAN_ARGS -var-file $file"
+done
+
+for file in "$module_path"/*.auto.tfvars.json; do
+    PLAN_ARGS="$PLAN_ARGS -var-file $file"
+done
+
 if [[ -n "<< parameters.var >>" ]]; then
     for var in $(echo "<< parameters.var >>" | tr ',' '\n'); do
         PLAN_ARGS="$PLAN_ARGS -var $var"
