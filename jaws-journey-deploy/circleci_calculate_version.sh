cc63094d97a074d9b8770f1ee5561175b35ce886
Chris.Ford <Chris.Ford@SPARKL370>
2020-10-12 14:07:53 +0100

    update sh commands

--- jaws-journey-deploy/circleci_calculate_version.sh
+++ jaws-journey-deploy/circleci_calculate_version.sh
@@ -1,2 +1,2 @@
-echo "export TF_VAR_release_version=${CIRCLE_TAG:-$CIRCLE_BRANCH-${CIRCLE_SHA1:0:8}}" >> $BASH_ENV
-echo "export TF_VAR_build_tag=${CIRCLE_SHA1}" >> $BASH_ENV
+export TF_VAR_release_version=${CIRCLE_TAG:-$CIRCLE_BRANCH-${CIRCLE_SHA1:0:8}}
+export TF_VAR_build_tag=${CIRCLE_SHA1}
