111e28eb23ddcf1504bd571c7460884d94e1f9a6
Chris.Ford <Chris.Ford@SPARKL370>
2020-10-12 13:33:27 +0100

    update sh commands

--- jaws-journey-deploy/circleci_set_profile.sh
+++ jaws-journey-deploy/circleci_set_profile.sh
@@ -1,3 +1,3 @@
 set -eu
-echo 'export PROFILE="<< parameters.environment >>"' >> $BASH_ENV
-echo "export VERSION=${CIRCLE_TAG:-$CIRCLE_BRANCH-${CIRCLE_SHA1:0:8}}" >> $BASH_ENV
+export PROFILE="<< parameters.environment >>"
+export VERSION=${CIRCLE_TAG:-$CIRCLE_BRANCH-${CIRCLE_SHA1:0:8}}"
