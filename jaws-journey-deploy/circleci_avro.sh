a178de3ccabde1260ad81924a9188061a51992dd
Chris.Ford <Chris.Ford@SPARKL370>
2020-10-12 12:46:39 +0100

    update integration tests sh

--- jaws-journey-deploy/circleci_avro.sh
+++ jaws-journey-deploy/circleci_avro.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -eu
-CD_PUBLISH=$1
 sudo chmod +x gradlew
 export AWS_REGION=${AWS_DEFAULT_REGION}
 
-if [[ ${CD_PUBLISH} == "true" ]]
+if [[ "<< parameters.uploadschema >>" == "true" ]]
 then
     ./gradlew avroUploadTask --full-stacktrace
     ./gradlew avroCheckCompatibleTask -PallowNotFound=false --full-stacktrace
