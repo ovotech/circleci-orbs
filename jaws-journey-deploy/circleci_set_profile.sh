9e30df6138ec71485f6a8c04a62b08871dc68f34
Chris.Ford <Chris.Ford@SPARKL370>
2020-10-12 13:00:29 +0100

    update avro step

--- jaws-journey-deploy/circleci_set_profile.sh
+++ jaws-journey-deploy/circleci_set_profile.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -eu
-echo ${PROFILE}
 echo 'export PROFILE="<< parameters.environment >>"' >> $BASH_ENV
 echo "export VERSION=${CIRCLE_TAG:-$CIRCLE_BRANCH-${CIRCLE_SHA1:0:8}}" >> $BASH_ENV
