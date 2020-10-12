e11bcc57c230c39e0a2b228a1053f1de391e8fc5
Chris.Ford <Chris.Ford@SPARKL370>
2020-10-12 12:21:49 +0100

    update integration tests sh

--- jaws-journey-deploy/circleci_run_integration_test.sh
+++ jaws-journey-deploy/circleci_run_integration_test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -eu
-SERVICE_NAME=$1
 sudo chmod +x gradlew
 #export AWS_REGION=${AWS_DEFAULT_REGION}
-./gradlew :${SERVICE_NAME}:integrationTest -Pprofile=${PROFILE} --full-stacktrace
+./gradlew :"<< parameters.serviceName >>":integrationTest -Pprofile=${PROFILE} --full-stacktrace
