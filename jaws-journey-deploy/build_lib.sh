8042595646deb0b75bc9c3857768c9e61f032995
Chris.Ford <Chris.Ford@SPARKL370>
2020-10-21 20:23:58 +0100

    fix parameter naming

--- jaws-journey-deploy/build_lib.sh
+++ jaws-journey-deploy/build_lib.sh
@@ -1,1 +1,1 @@
-./gradlew :"<< parameters.v >>":clean :"<< parameters.lib >>":buildNeeded  -Pprofile=${PROFILE} -x integrationTest --full-stacktrace lib: "<< parameters.lib >>"
+./gradlew :"<< parameters.lib >>":clean :"<< parameters.lib >>":buildNeeded  -Pprofile=${PROFILE} -x integrationTest --full-stacktrace lib: "<< parameters.lib >>"
