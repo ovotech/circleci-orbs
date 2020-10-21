54014d1d61517c562666d371da7dc3d2c2e530fa
Chris.Ford <Chris.Ford@SPARKL370>
2020-10-21 19:41:00 +0100

    change how test results are saved

--- jaws-journey-deploy/save_test_results.sh
+++ jaws-journey-deploy/save_test_results.sh
@@ -1,16 +1,18 @@
+
 SERVICE_NAME="<< parameters.serviceName >>"
 IS_LIB="<< parameters.save_libs >>"
+
 mkdir -p ~/test-results/junit/
 find . -type f -regex ".*/build/test-results/.*xml" -exec cp {} ~/test-results/junit/ \;
-
+mkdir -p ./reports/jacoco/
 mkdir -p ./reports/jacoco/"$SERVICE_NAME"/
-mv "$SERVICE_NAME"/build/jacoco-reports/test/jacocoTestReport.csv ./reports/jacoco/"$SERVICE_NAME".csv
-mv "$SERVICE_NAME"/build/jacoco-reports/test/* ./reports/jacoco/"$SERVICE_NAME"
 
+REPORT_FOLDER=""
 if [[ "$IS_LIB" = true ]] ; then
-  for proj in $(find . -type f -regex "./libs/.*/build/jacoco-reports/test/jacocoTestReport.csv" | cut -d/ -f3); do
-    mkdir -p ./reports/jacoco/"$proj"/
-    mv libs/"$proj"/build/jacoco-reports/test/jacocoTestReport.csv ./reports/jacoco/"$proj".csv
-    mv libs/"$proj"/build/jacoco-reports/test/* ./reports/jacoco/"$proj"
-  done
+  REPORT_FOLDER="libs/$SERVICE_NAME/build/jacoco-reports/test"
+else
+  REPORT_FOLDER="$SERVICE_NAME/build/jacoco-reports/test"
 fi
+
+mv "$REPORT_FOLDER"/jacocoTestReport.csv ./reports/jacoco/"$SERVICE_NAME".csv
+mv "$REPORT_FOLDER"/* ./reports/jacoco/"$SERVICE_NAME"
