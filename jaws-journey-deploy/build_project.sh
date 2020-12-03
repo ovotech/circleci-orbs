7a7aecf37ca875ab095b7e79189a124232d05f83
Daniel Malý <daniel.maly@kaluza.com>
2020-12-03 13:49:10 +0000

    Change gradleParams to skipUnitTests

--- jaws-journey-deploy/build_project.sh
+++ jaws-journey-deploy/build_project.sh
@@ -1,1 +1,9 @@
-./gradlew :"<< parameters.serviceName >>":clean :"<< parameters.serviceName >>":buildNeeded -Pprofile=${PROFILE} -x integrationTest --full-stacktrace << parameters.gradleParams >>
+SKIP_UNIT_TESTS="<< parameters.skipUnitTests >>"
+
+if [[ "$SKIP_UNIT_TESTS" = true ]] ; then
+  EXCLUDE="-x test"
+else
+  EXCLUDE=""
+fi
+
+./gradlew :"<< parameters.serviceName >>":clean :"<< parameters.serviceName >>":buildNeeded -Pprofile=${PROFILE} -x integrationTest --full-stacktrace ${EXCLUDE}
