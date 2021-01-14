cb4a5ae3c903542046cba7879145e16adfdfba7c
chrisford <chris.ford@kaluza.com>
2021-01-14 11:58:07 +0000

    add build option for integration tests not existing in source code - common-libs

--- jaws-journey-deploy/build_lib.sh
+++ jaws-journey-deploy/build_lib.sh
@@ -1,1 +1,6 @@
-./gradlew :libs:"<< parameters.lib >>":clean :libs:"<< parameters.lib >>":buildNeeded  -Pprofile=${PROFILE} -x integrationTest --full-stacktrace
+CONTAINS_INTEGRATION_TESTS="<< parameters.contains_integration_tests >>"
+if [[ "CONTAINS_INTEGRATION_TESTS" = true ]] ; then
+  ./gradlew :libs:"<< parameters.lib >>":clean :libs:"<< parameters.lib >>":buildNeeded  -Pprofile=${PROFILE} -x integrationTest --full-stacktrace
+else
+  ./gradlew :libs:"<< parameters.lib >>":clean :libs:"<< parameters.lib >>":buildNeeded --full-stacktrace
+fi
