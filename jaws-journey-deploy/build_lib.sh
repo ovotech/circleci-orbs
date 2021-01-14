2960f97eafbdb4b039dd0ac06f2024cc2c1bb0c8
chrisford <chris.ford@kaluza.com>
2021-01-14 12:44:38 +0000

    update build command

--- jaws-journey-deploy/build_lib.sh
+++ jaws-journey-deploy/build_lib.sh
@@ -2,5 +2,5 @@ CONTAINS_INTEGRATION_TESTS="<< parameters.contains_integration_tests >>"
 if [[ "CONTAINS_INTEGRATION_TESTS" = true ]] ; then
   ./gradlew :libs:"<< parameters.lib >>":clean :libs:"<< parameters.lib >>":buildNeeded  -Pprofile=${PROFILE} -x integrationTest --full-stacktrace
 else
-  ./gradlew :libs:"<< parameters.lib >>":clean :libs:"<< parameters.lib >>":buildNeeded --full-stacktrace
+  ./gradlew :libs:"<< parameters.lib >>":clean :libs:"<< parameters.lib >>":build --full-stacktrace
 fi
