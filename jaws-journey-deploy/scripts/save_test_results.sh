f524265d2e82e675bf57f69eca4f7511cef98a73
chrisford <chris.ford@kaluza.com>
2021-02-10 15:13:32 +0000

    add topology describe to github comment

--- jaws-journey-deploy/scripts/save_test_results.sh
+++ jaws-journey-deploy/scripts/save_test_results.sh
@@ -27,7 +27,7 @@ DIAGRAM_FOLDER=~/test-results/junit/
 mkdir -p ./reports/diagrams/
 
 if [ -e "$DIAGRAM_FOLDER"/journeyCotTopologyDiagram.png ]; then
-  mv "$DIAGRAM_FOLDER"/journeyCotTopologyDiagram.png ./reports/diagrams/TopologyDiagram.png
+  mv "$DIAGRAM_FOLDER"/journeyCotTopologyDiagram.png ./reports/diagrams/topologyDiagram.png
   mv "$DIAGRAM_FOLDER"/* ./reports/diagrams
 else
    echo file does not exist or is not executable
