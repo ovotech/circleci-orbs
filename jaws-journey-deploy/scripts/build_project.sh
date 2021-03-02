b82cd5a7f038f18c67671330e913b13599963db5
chrisford <chris.ford@kaluza.com>
2021-03-02 16:38:21 +0000

    update switch no daemon

--- jaws-journey-deploy/scripts/build_project.sh
+++ jaws-journey-deploy/scripts/build_project.sh
@@ -7,7 +7,7 @@ else
 fi
 
 if [[ "$SAVE_TOPOLOGY" = true ]] ; then
-  ./gradlew :"<< parameters.serviceName >>":clean :"<< parameters.serviceName >>":saveTopologyTask -Pprofile=${PROFILE} -x integrationTest --full-stacktrace ${EXCLUDE}
+  ./gradlew :"<< parameters.serviceName >>":clean :"<< parameters.serviceName >>":saveTopologyTask -Pprofile=${PROFILE} --no-daemon -x integrationTest --full-stacktrace ${EXCLUDE}
 else
-  ./gradlew :"<< parameters.serviceName >>":clean :"<< parameters.serviceName >>":build -Pprofile=${PROFILE} -x integrationTest --full-stacktrace ${EXCLUDE}
+  ./gradlew :"<< parameters.serviceName >>":clean :"<< parameters.serviceName >>":build -Pprofile=${PROFILE} --no-daemon -x integrationTest --full-stacktrace ${EXCLUDE}
 fi
