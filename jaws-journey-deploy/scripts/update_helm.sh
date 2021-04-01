bc8dea9fc64e0aa0325ee06b814da57dae9e2949
Reka Elek <reka.elek@ovoenergy.com>
2021-04-01 12:57:22 +0100

    update helm fixes

---
+++ jaws-journey-deploy/scripts/update_helm.sh
@@ -0,0 +1,8 @@
+yq e ".buildTag=\"${CIRCLE_SHA1}\"" -i <<parameters.manifest_directory>>/values-<<parameters.environment>>.yaml
+yq e ".releaseVersion=\"${CIRCLE_TAG:-$CIRCLE_BRANCH-${CIRCLE_SHA1:0:8}}\"" -i <<parameters.manifest_directory>>/values-<<parameters.environment>>.yaml
+
+git clone
+git config user.name '<<parameters.git_name>>'
+git config user.email '<<parameters.git_email>>'
+git commit -m "[skip ci] CircleCI deploy with helm ${CIRCLE_PROJECT_REPONAME}" -m  "Build URL: ${CIRCLE_BUILD_URL}" -a
+git push origin << parameters.deploy_branch >>
