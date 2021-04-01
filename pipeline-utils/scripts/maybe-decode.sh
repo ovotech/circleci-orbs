3cd3a734b30eaaa5833d52f933364b03b62217b5
Chris Brindley <chris.brindley@ovoenergy.com>
2021-04-01 14:17:31 +0100

    Be consistent with how scripts interact with $BASH_ENV

--- pipeline-utils/scripts/maybe-decode.sh
+++ pipeline-utils/scripts/maybe-decode.sh
@@ -2,16 +2,17 @@
 # If the input appears to be a valid base64 string then decode it, otherwise
 # return it as is
 # Args:
-#  * $1 - Potentially base64 encoded string
+#  * $1 - Potentially base64 encoded env var
 maybe_decode () {
-  local _VAL=${1}
+  local _VAR=${1}
+  local _VAL=""
+
+  _VAL="$(printenv "${_VAR}")"
 
   # Test that the value appears to be a base64 encoded string. If not, return
-  # the value immediately
   if ! (grep -E '^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$' <<< "${_VAL}" >/dev/null 2>&1); then
-    echo "${_VAL}"
     return 0
   fi
 
-  echo "${_VAL}" | base64 -d
+  echo "export ${_VAR}='$(echo "${_VAL}" | base64 -d)'" >> "${BASH_ENV}"
 }
