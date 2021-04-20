c7c5b88adffeee753f5fded49e1c98dfa6541114
Eleanor Nicholson <eleanor.nicholson@kaluza.com>
2021-04-20 17:24:52 +0100

    Fix bug in new workspace

--- terraform-v2/new_workspace.sh
+++ terraform-v2/new_workspace.sh
@@ -1,5 +1,6 @@
 set -x
-iif terraform -chdir=${module_path} workspace list -no-color | grep "$workspace" >/dev/null; then
+
+if terraform -chdir=${module_path} workspace list -no-color | grep "$workspace" >/dev/null; then
   terraform -chdir=${module_path} workspace select -no-color "$workspace"
 else
   terraform -chdir=${module_path} workspace new -no-color -lock-timeout=300s "$workspace"
