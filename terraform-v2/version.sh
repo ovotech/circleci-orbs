5fbd34d6e8f524430d7eab1d87d85841c51a330a
Eleanor Nicholson <eleanor.nicholson@kaluza.com>
2021-04-23 10:27:27 +0100

    Remove failing -input param from terraform version

--- terraform-v2/version.sh
+++ terraform-v2/version.sh
@@ -2,4 +2,4 @@ set -x
 
 rm -rf ${module_path}/.terraform
 terraform -chdir=${module_path} init -input=false -backend=false -no-color
-terraform version -input=false -no-color
+terraform version -no-color
