713743a285f15b6f0d4c7ea6671b0a93489d460b
Stephen Harris <stephen.harris1@kaluza.com>
2021-05-26 09:17:21 +0100

    Implement retry
    Resolves race condition where if two or more workflows are running at the same time
    (i.e. nonprod & test) one might commit manifest changes after another has checked-out
    the repo which prevents the latter from commiting their manifest changes.

--- jaws-journey-deploy/scripts/update_helm.sh
+++ jaws-journey-deploy/scripts/update_helm.sh
@@ -1,10 +1,35 @@
-git clone git@github.com:${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} /tmp/gitops
-cd /tmp/gitops
+MAX_RETRY=3
+COUNTER=0
 
-yq e ".jaws-journey-helm-chart.buildTag=\"${CIRCLE_SHA1}\"" -i <<parameters.manifest_directory>>/values-<<parameters.environment>>.yaml
-yq e ".jaws-journey-helm-chart.releaseVersion=\"${CIRCLE_TAG:-$CIRCLE_BRANCH-${CIRCLE_SHA1:0:8}}\"" -i <<parameters.manifest_directory>>/values-<<parameters.environment>>.yaml
+function deply_manifest {
 
-git config user.name '<<parameters.git_name>>'
-git config user.email '<<parameters.git_email>>'
-git commit -m "[skip ci] CircleCI deploy with helm ${CIRCLE_PROJECT_REPONAME}" -m  "Build URL: ${CIRCLE_BUILD_URL}" -a
-git push origin master
+  # Ensure /tmp/gitops is empty
+  cd ~/
+  rm -rf /tmp/gitops
+  mkdir -p /tmp/gitops
+  
+  # Clone manifest repo
+  git clone git@github.com:${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} /tmp/gitops
+  cd /tmp/gitops
+  
+  # Update helm chart
+  yq e ".jaws-journey-helm-chart.buildTag=\"${CIRCLE_SHA1}\"" -i <<parameters.manifest_directory>>/values-<<parameters.environment>>.yaml
+  yq e ".jaws-journey-helm-chart.releaseVersion=\"${CIRCLE_TAG:-$CIRCLE_BRANCH-${CIRCLE_SHA1:0:8}}\"" -i <<parameters.manifest_directory>>/values-<<parameters.environment>>.yaml
+  
+  # Commit manifest changes
+  git config user.name '<<parameters.git_name>>'
+  git config user.email '<<parameters.git_email>>'
+  git commit -m "[skip ci] CircleCI deploy with helm ${CIRCLE_PROJECT_REPONAME}" -m  "Build URL: ${CIRCLE_BUILD_URL}" -a
+  git push origin master
+
+  return $?
+}
+
+
+until deply_manifest
+do
+   sleep 1
+   [[ COUNTER -eq $MAX_RETRY ]] && echo "Failed!" && exit 1
+   echo "Trying again. Try #$COUNTER"
+   ((COUNTER++))
+done
