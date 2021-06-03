7c5607f4c2635561afc815608bf95206423f78ce
Stephen Harris <stephen.harris1@kaluza.com>
2021-06-03 12:54:28 +0100

    Add lines after script

--- argocd/scripts/wait_for_sync.sh
+++ argocd/scripts/wait_for_sync.sh
@@ -3,4 +3,3 @@ include wait_for_sync.py
 EOF
 
 python3 /tmp/wait_for_sync.py --wait-for=<<parameters.wait_for>> --application=<<parameters.application>> --target=<<parameters.target>> --argocd-url <<parameters.argocd_url>>
-      
