a55869b11f444e52f4064b6e2544150d15120385
chrisford <chris.ford@kaluza.com>
2021-06-15 20:01:18 +0100

    add flag to run integration tests

--- jaws-journey-deploy/scripts/build_project.sh
+++ jaws-journey-deploy/scripts/build_project.sh
@@ -1,6 +1,7 @@
 SKIP_UNIT_TESTS="<< parameters.skipUnitTests >>"
 SAVE_TOPOLOGY="<< parameters.saveTopology >>"
 RUN_INTEGRATION_TEST="<< parameters.runIntegrationTest >>"
+SERVICE="<< parameters.serviceName >>"
 
 if [[ "$SKIP_UNIT_TESTS" = true ]] ; then
   EXCLUDE="-x test"
@@ -9,13 +10,13 @@ else
 fi
 
 if [[ "RUN_INTEGRATION_TEST" = true ]] ; then
-  INTEGRATION_TEST=":< parameters.serviceName >>:integrationTest"
+  INTEGRATION_TEST=":$SERVICE:integrationTest"
 else
   INTEGRATION_TEST="-x integrationTest"
 fi
 
 if [[ "$SAVE_TOPOLOGY" = true ]] ; then
-  ./gradlew :"<< parameters.serviceName >>":clean :"<< parameters.serviceName >>":saveTopologyTask -Pprofile=${PROFILE} --no-daemon --full-stacktrace "${EXCLUDE}" "${INTEGRATION_TEST}"
+  ./gradlew :"<< parameters.serviceName >>":clean :"$SERVICE":saveTopologyTask -Pprofile="${PROFILE}" --no-daemon --full-stacktrace "${EXCLUDE}" "${INTEGRATION_TEST}"
 else
-  ./gradlew :"<< parameters.serviceName >>":clean :"<< parameters.serviceName >>":build -Pprofile=${PROFILE} --no-daemon --full-stacktrace "${EXCLUDE}" "${INTEGRATION_TEST}"
+  ./gradlew :"<< parameters.serviceName >>":clean :"$SERVICE":build -Pprofile="${PROFILE}" --no-daemon --full-stacktrace "${EXCLUDE}" "${INTEGRATION_TEST}"
 fi
