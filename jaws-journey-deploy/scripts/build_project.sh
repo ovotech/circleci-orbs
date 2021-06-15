ed16f3d7a21158be14bad3be6bfe809582a4adc4
chrisford <chris.ford@kaluza.com>
2021-06-15 20:24:43 +0100

    add flag to run integration tests

--- jaws-journey-deploy/scripts/build_project.sh
+++ jaws-journey-deploy/scripts/build_project.sh
@@ -1,7 +1,6 @@
 SKIP_UNIT_TESTS="<< parameters.skipUnitTests >>"
 SAVE_TOPOLOGY="<< parameters.saveTopology >>"
 RUN_INTEGRATION_TEST="<< parameters.runIntegrationTest >>"
-SERVICE="<< parameters.serviceName >>"
 
 if [[ "$SKIP_UNIT_TESTS" = true ]] ; then
   EXCLUDE="-x test"
@@ -11,14 +10,14 @@ fi
 
 if [[ "$SAVE_TOPOLOGY" = true ]] ; then
   if [[ "$RUN_INTEGRATION_TEST" = true ]] ; then
-    ./gradlew :"$SERVICE":clean :"$SERVICE":saveTopologyTask :"$SERVICE":integrationTest -Pprofile="${PROFILE}" --no-daemon --full-stacktrace "${EXCLUDE}"
+    ./gradlew :"<< parameters.serviceName >>":clean :"<< parameters.serviceName >>":saveTopologyTask :"<< parameters.serviceName >>":integrationTest -Pprofile="${PROFILE}" --no-daemon --full-stacktrace "${EXCLUDE}"
   else
-    ./gradlew :"$SERVICE":clean :"$SERVICE":saveTopologyTask -Pprofile="${PROFILE}" --no-daemon --full-stacktrace "${EXCLUDE}" -x integrationTest
+    ./gradlew :"$SERVICE":clean :"<< parameters.serviceName >>":saveTopologyTask -Pprofile="${PROFILE}" --no-daemon --full-stacktrace "${EXCLUDE}" -x integrationTest
   fi
 else
   if [[ "$RUN_INTEGRATION_TEST" = true ]] ; then
-    ./gradlew :"$SERVICE":clean :"$SERVICE":build :"$SERVICE":integrationTest -Pprofile="${PROFILE}" --no-daemon --full-stacktrace "${EXCLUDE}"
+    ./gradlew :"<< parameters.serviceName >>":clean :"<< parameters.serviceName >>":build :"<< parameters.serviceName >>":integrationTest -Pprofile="${PROFILE}" --no-daemon --full-stacktrace "${EXCLUDE}"
   else
-    ./gradlew :"$SERVICE":clean :"$SERVICE":build -Pprofile="${PROFILE}" --no-daemon --full-stacktrace "${EXCLUDE}" -x integrationTest
+    ./gradlew :"<< parameters.serviceName >>":clean :"<< parameters.serviceName >>":build -Pprofile="${PROFILE}" --no-daemon --full-stacktrace "${EXCLUDE}" -x integrationTest
   fi
 fi
