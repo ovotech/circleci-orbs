905431057ff06014be02b61e83ef558ccc492ca1
Ewan Morrison <ewanmorrrison@gmail.com>
2021-06-22 16:12:14 +0100

    Fix passing of args to python script

--- jaws-journey-deploy/scripts/cleanup_kafka.sh
+++ jaws-journey-deploy/scripts/cleanup_kafka.sh
@@ -2,4 +2,8 @@ cat >/tmp/cleanup_kafka.py <<"EOF"
 include cleanup_kafka.py
 EOF
 
-python3 /tmp/cleanup_kafka.py -a "$1" -v "$2" -k "$3" -d "$4"
+python3 /tmp/cleanup_kafka.py \
+  --app_id_common "<< parameters.app-id-common >>" \
+  --app_id_version "<< parameters.app-id-version >>" \
+  --kube_namespace "<< parameters.kube-namespace >>" \
+  --dry_run "<< parameters.preview-mode >>"
