cf83b1cffd70daa2bd4c9abf36f780ac4cede2ac
Drum Ogilvie <drum.ogilvie@ovoenergy.com>
2021-08-24 14:50:43 +0100

    Use difflib for a slightly more helpful cmp.py
    Happy to revert this after debugging is done

--- terraform-v2/cmp.py
+++ terraform-v2/cmp.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
+import difflib
 import sys
 
 with open(sys.argv[1], encoding='utf-8') as f:
-    first = f.read()
+    generated_plan = f.readlines()
 with open(sys.argv[2], encoding='utf-8') as f:
-    second = f.read()
-exit(0 if first.strip() == second.strip() else 1)
+    plan_from_pr = f.readlines()
+
+diff = list(difflib.ndiff(generated_plan, plan_from_pr))
+non_matching_lines = [line for line in diff if line[0] != ' ']
+if non_matching_lines:
+    print("Plan has changed!")
+    print("".join(diff))
+    exit(1)
+else:
+    exit(0)
