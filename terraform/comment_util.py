20e16f2a82664403e5134e7a998bc3c84cc7361a
Chris Brindley <chris.brindley@ovoenergy.com>
2021-11-01 15:52:27 +0000

    Update comment_match to account for new comment formatting

--- terraform/comment_util.py
+++ terraform/comment_util.py
@@ -3,6 +3,13 @@ import re
 
 def re_comment_match(comment_id, comment_body):
     """Returns a Match object, or None if no match was found"""
+    m = re.match(rf'{re.escape(comment_id)}\n<details>\n<summary>View Terraform Plan</summary>\n\n'
+                 rf'```terraform\nOutput is limited to 1000 lines and may be truncated\. '
+                 rf'See CircleCI for full details\.\n(.*)\n```\n</details>\n(.*)',
+                 comment_body, re.DOTALL)
+    if m is not None:
+        return m
+
     return re.match(rf'{re.escape(comment_id)}\n```(?:hcl)?(.*?)```(.*)',
                     comment_body, re.DOTALL)
 
