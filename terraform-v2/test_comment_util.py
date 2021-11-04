fd22ea382b90de94c12c2d47a45a9d14e21452cb
chris-brindley <67270396+chris-brindley@users.noreply.github.com>
2021-11-04 15:23:56 +0000

    Re-enable terraform orb collapsible plans (#354)
    * Update comment_util.py tests and comment formatting
    * Add python testing to the CI for terraform orbs
    * Remove old comment template

--- terraform-v2/test_comment_util.py
+++ terraform-v2/test_comment_util.py
@@ -4,20 +4,22 @@ import pytest
 
 @pytest.mark.parametrize("comment_id,comment_body,match_group_one,match_group_two",
                          [
-                            #  pre addition of HCL syntax formatting
-                             ("<comment_id>",
-                              "<comment_id>\n```\n<plan>\n```<status>",
-                              "<plan>",
-                              "<status>"
-                             ),
                             #  post addition of HCL syntax formatting
                              ("<comment_id>",
                               "<comment_id>\n```hcl\n<plan>\n```<status>",
                               "<plan>",
                               "<status>"
                              ),
+                            #  post addition of collapsible formatting
+                             ("<comment_id>",
+                              "<comment_id>\n<details>\n<summary>View Terraform "
+                              "Plan</summary>\n\n```terraform\nOutput is limited to 1000 lines and may be truncated. "
+                              "See CircleCI for full details.\n<plan>\n```\n</details>\n<status>",
+                              "<plan>",
+                              "<status>"
+                             ),
                           ])
-def test_regex_comment_match(comment_id, comment_body, 
+def test_regex_comment_match(comment_id, comment_body,
                              match_group_one, match_group_two):
     match = comment_util.re_comment_match(comment_id, comment_body)
     assert match.group(1).strip() == match_group_one
@@ -26,4 +28,15 @@ def test_regex_comment_match(comment_id, comment_body,
 
 def test_comment_for_pr():
     comment_for_pr = comment_util.comment_for_pr("<comment_id>", "<plan>")
-    assert comment_for_pr == "<comment_id>\n```hcl\n<plan>\n```"
+    assert comment_for_pr == ("<comment_id>\n<details>\n<summary>View Terraform "
+                              "Plan</summary>\n\n```terraform\nOutput is limited to 1000 lines and may be truncated. "
+                              "See CircleCI for full details.\n<plan>\n```\n</details>\n")
+
+
+def test_comment_match_decodes_comment_for_pr():
+    id = "<comment_id>"
+    plan = "<plan>"
+    comment_for_pr = comment_util.comment_for_pr(id, plan)
+    match = comment_util.re_comment_match(id, comment_for_pr)
+    assert match.group(1).strip() == plan
+    assert match.group(2).strip() == ""
