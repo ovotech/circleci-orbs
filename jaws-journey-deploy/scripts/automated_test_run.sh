c9c694a14e535df86f5ee22013d7f1a5002aabec
Sam Cooper <66436650+samcooper720x@users.noreply.github.com>
2021-12-06 12:50:36 +0000

    feat: Make service name a parameter for run-automation-test job

--- jaws-journey-deploy/scripts/automated_test_run.sh
+++ jaws-journey-deploy/scripts/automated_test_run.sh
@@ -1,3 +1,3 @@
 export AWS_REGION=${AWS_DEFAULT_REGION}
-export SERVICE_NAME=automated-test
+export SERVICE_NAME=$1
 ./gradlew :${SERVICE_NAME}:test -Pprofile=${PROFILE} --full-stacktrace -PrunEndToEndTests=true
