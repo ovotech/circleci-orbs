6642fad866db943944db69449279c6bf23949a1c
Sam Cooper <66436650+samcooper720x@users.noreply.github.com>
2021-12-07 10:23:14 +0000

    Try something

--- jaws-journey-deploy/scripts/automated_test_run.sh
+++ jaws-journey-deploy/scripts/automated_test_run.sh
@@ -1,2 +1,3 @@
 export AWS_REGION=${AWS_DEFAULT_REGION}
+export AUTOMATED_TEST_SUITE=${AUTOMATED_TEST_SUITE}
 ./gradlew :${AUTOMATED_TEST_SUITE}:test -Pprofile=${PROFILE} --full-stacktrace -PrunEndToEndTests=true
