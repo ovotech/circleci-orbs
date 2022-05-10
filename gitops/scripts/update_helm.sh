e0b28bc82277a253a1340fb6f0d528f5ef6ce780
Kathryn.Allan <kathryn.allan@kaluza.com>
2022-05-10 12:56:58 +0100

    Fix

--- gitops/scripts/update_helm.sh
+++ gitops/scripts/update_helm.sh
@@ -18,10 +18,10 @@ function deply_manifest {
   # Clone manifest repo
   git clone -b <<parameters.manifest_branch>> git@github.com:${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} /tmp/gitops
   cd /tmp/gitops
-  
+
   # Update helm chart
-  if [[ << parameters.image_tag >> != "" ]] ; then
-    yq e "<<parameters.image_tag_yaml_path>>=\"<<parameters.image_tag>>\"" -i $VALUES_FILE || exit 1
+  if [[ << parameters.image_tag >> != "" ]]; then
+    yq e "<<parameters.image_tag_yaml_path>>=\"<<parameters.image_tag>>\"" -i $VALUES_FILE
   
     # Commit manifest changes
     git config user.name '<<parameters.git_name>>'
