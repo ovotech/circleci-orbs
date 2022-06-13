d55e5a3e9df5b003f5e7c47a16c22a43da1bccc0
Kathryn.Allan <kathryn.allan@kaluza.com>
2022-06-13 08:25:03 +0100

    Adding missing export

--- jaws-journey-deploy/scripts/automated_test_run.sh
+++ jaws-journey-deploy/scripts/automated_test_run.sh
@@ -1,4 +1,5 @@
 export AWS_REGION=${AWS_DEFAULT_REGION}
 export AUTOMATED_TEST_SUITE="<< parameters.automatedTestSuite >>"
 export DEPLOYMENT="<< parameters.deployment >>"
+export IS_PRIMARY_DEPLOYMENT="<< parameters.isPrimaryDeployment >>"
 ./gradlew :${AUTOMATED_TEST_SUITE}:test -Pprofile=${PROFILE} --full-stacktrace -PrunEndToEndTests=true
