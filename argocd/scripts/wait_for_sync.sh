970673338487f6ce0706abaacd5ab26da4e521e4
RostamKal <rostam.saebnoori@kaluza.com>
2022-11-04 15:54:18 +0000

    flag for sync request

--- argocd/scripts/wait_for_sync.sh
+++ argocd/scripts/wait_for_sync.sh
@@ -2,4 +2,8 @@ cat >/tmp/wait_for_sync.py <<"EOF"
 include wait_for_sync.py
 EOF
 
-python3 /tmp/wait_for_sync.py --wait-for=<<parameters.wait_for>> --application=<<parameters.application>> --target=<<parameters.target>> --argocd-url <<parameters.argocd_url>> --sync-request <<parameters.sync_request>>
+if [ <<parameters.sync-request>> == 'True' ]; then
+    python3 /tmp/wait_for_sync.py --wait-for=<<parameters.wait_for>> --application=<<parameters.application>> --target=<<parameters.target>> --argocd-url <<parameters.argocd_url>> --sync-request
+else
+    python3 /tmp/wait_for_sync.py --wait-for=<<parameters.wait_for>> --application=<<parameters.application>> --target=<<parameters.target>> --argocd-url <<parameters.argocd_url>>
+fi
