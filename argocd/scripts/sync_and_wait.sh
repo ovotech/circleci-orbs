06c41aa5ed39e91c1e33be6b6b35ac0b87beb255
RostamKal <rostam.saebnoori@kaluza.com>
2022-11-04 14:05:51 +0000

    refactor, using parameter for sync request

--- argocd/scripts/sync_and_wait.sh
+++
@@ -1,5 +0,0 @@
-cat >/tmp/sync_and_wait.py <<"EOF"
-include sync_and_wait.py
-EOF
-
-python3 /tmp/sync_and_wait.py  --wait-for=<<parameters.wait_for>>  --target=<<parameters.target>>  --application=<<parameters.application>>  --argocd-url <<parameters.argocd_url>>
--- argocd/scripts/wait_for_sync.sh
+++ argocd/scripts/wait_for_sync.sh
@@ -2,4 +2,4 @@ cat >/tmp/wait_for_sync.py <<"EOF"
 include wait_for_sync.py
 EOF
 
-python3 /tmp/wait_for_sync.py --wait-for=<<parameters.wait_for>> --application=<<parameters.application>> --target=<<parameters.target>> --argocd-url <<parameters.argocd_url>>
+python3 /tmp/wait_for_sync.py --wait-for=<<parameters.wait_for>> --application=<<parameters.application>> --target=<<parameters.target>> --argocd-url <<parameters.argocd_url>> --sync-request <<parameters.sync_request>>
