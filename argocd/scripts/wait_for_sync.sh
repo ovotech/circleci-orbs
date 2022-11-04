789211b8077f2df7d1a38473ca1e6e0214d1989f
RostamKal <rostam.saebnoori@kaluza.com>
2022-11-04 16:00:57 +0000

    var correction

--- argocd/scripts/wait_for_sync.sh
+++ argocd/scripts/wait_for_sync.sh
@@ -2,7 +2,7 @@ cat >/tmp/wait_for_sync.py <<"EOF"
 include wait_for_sync.py
 EOF
 
-if [ <<parameters.sync-request>> == 'True' ]; then
+if [ <<parameters.sync_request>> == 'True' ]; then
     python3 /tmp/wait_for_sync.py --wait-for=<<parameters.wait_for>> --application=<<parameters.application>> --target=<<parameters.target>> --argocd-url <<parameters.argocd_url>> --sync-request
 else
     python3 /tmp/wait_for_sync.py --wait-for=<<parameters.wait_for>> --application=<<parameters.application>> --target=<<parameters.target>> --argocd-url <<parameters.argocd_url>>
