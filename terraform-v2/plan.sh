14bf4a4ac32a8c31022c48e4b770f7fe7bf66671
shashank.gupta <shashank.gupta@ovo.com>
2023-05-31 13:43:21 +0100

    changing to module path before generating plan.txt

--- terraform-v2/plan.sh
+++ terraform-v2/plan.sh
@@ -15,11 +15,11 @@ exec 3>&1
 set +e
 terraform -chdir=${module_path} plan -input=false -no-color -detailed-exitcode -lock-timeout=300s -out=plan.out $PLAN_ARGS
 
-cd ${module_path} && terraform show -no-color plan.out \
+(cd ${module_path} && terraform show -no-color plan.out \
     | $TFMASK \
     | tee /dev/fd/3 \
     | $COMPACT_PLAN \
-    > plan.txt
+) > plan.txt
 
 readonly TF_EXIT=${PIPESTATUS[0]}
 set -e
